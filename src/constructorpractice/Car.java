8c1740f1d79884da1f638438709a4db35ca115eb
AbdulMoizChishti <moaiz599@gmail.com>
2021-09-21 04:15:45 +0500

    Constructor Practice

---
+++ src/constructorpractice/Car.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constructorpractice;
+
+/**
+ *
+ * @author Abdul Moiz Chishti
+ */
+public class Car {
+    private int rn=0;
+    Car(String name,int model, String clr){
+        
+        
+        System.out.println("Enter Car name:"+name);
+        System.out.println("Enter model:"+model);
+        System.out.println("Enter Color:"+clr);
+    
+        System.out.println(rn);
+    
+    
+    
+    }
+
+
+
+
+}
---
+++ src/constructorpractice/ConstructorPractice.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constructorpractice;
+
+/**
+ *
+ * @author Abdul Moiz Chishti
+ */
+public class ConstructorPractice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Car obj=new Car("Hyundai",2010,"red");
+    
+   
+    }
+    
+}
